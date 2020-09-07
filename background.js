44dd89b45840404fb1b1f09059e46f411aad8a27
Shubbler <shubbler@me.com>
2020-09-07 02:36:15 +0100

    Commented logic

--- background.js
+++ background.js
@@ -10,12 +10,15 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     const url = new URL(details.url);
     
+    // If already requesting old reddit, continue
     if (url.hostname === "old.reddit.com") return;
     
+    // If user is attempting to access a redesign view, continue
     for (const path of excludedPaths) {
       if (url.pathname.indexOf(path) === 0) return;
     }
     
+    // Finally return a URL redirecting user back to old Reddit
     return {redirectUrl: oldReddit + url.pathname + url.search + url.hash};
   },
   {
